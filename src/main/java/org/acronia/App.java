3b5a32d32f38625f58480a27e9f57aa962496796
rotensyo <60921173+rotensyo@users.noreply.github.com>
2020-02-14 00:45:31 +0900

    不要部分を削除

--- src/main/java/org/acronia/App.java
+++ src/main/java/org/acronia/App.java
@@ -13,38 +13,12 @@ import org.acronia.controller.Controller;
  */
 public class App extends Application {
 
-    /*
-    private static Scene scene;
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("eco_searcher_template"));
-        stage.setScene(scene);
-        stage.setTitle("アイテムカタログ(アクロニア) Ver.1.0");
-        stage.setResizable(true);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        fxmlLoader.setController(new Controller());
-        return fxmlLoader.load();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-    */
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new  FXMLLoader(getClass().getResource("eco_searcher_template.fxml"));
         loader.setController(new Controller());
         Parent root = loader.load();
-        primaryStage.setTitle("アイテムカタログ(アクロニア) Ver.1.0");
+        primaryStage.setTitle("アイテムカタログ(アクロニア) Ver.1.1");
         Image icon = new Image(getClass().getResourceAsStream("icon.png"));
         primaryStage.getIcons().add( icon );
         primaryStage.setScene(new Scene(root));
